aa94b3625a48f68af77fe76ede1137155317b8b5
user <user@DESKTOP-GPH9M42>
2018-09-12 23:56:49 +0300

    No real change

--- AttributeParser/AttributeParser.cpp
+++ AttributeParser/AttributeParser.cpp
@@ -90,6 +90,7 @@ void AttributeParser::clear_unwanted_charecters(std::string &k)
 	k.erase(std::remove(k.begin(), k.end(), '>'), k.end());
 }
 
+
 AttributeParserNode* AttributeParser::find_node_by_tag_name(const std::string& tag_name)
 {
 	AttributeParserNode* node_ptr = NULL;
